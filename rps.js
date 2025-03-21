cfb3bbbc86f27d45158e4ff1cf918614cd02ff52
Billy Leader <164017159+bleader14@users.noreply.github.com>
2025-03-21 14:48:27 +0000

    Add Player input and check against computer
    This revision has added the 'getPlayerChoice' function which takes the
    user input and compares it against the computers random decision
    returning a message on who has won

--- rps.js
+++ rps.js
@@ -18,4 +18,35 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("Make your choice");
+    let compChoice = getComputerChoice();
+
+    if (humanChoice === compChoice) {
+        return "draw";
+    }
+
+    switch (humanChoice) {
+        case "Rock":
+            if (compChoice === "Paper") {
+                return "Computer wins";
+            } else {
+                return "User wins";
+            }
+        case "Paper":
+            if (compChoice === "Scissors") {
+                return "Computer wins";
+            } else {
+                return "User wins";
+            }
+        case "Scissors":
+            if (compChoice === "Rock") {
+                return "Computer wins";
+            } else {
+                return "User wins";
+            } 
+        default:
+            return "No valid option entered";
+    }
+}
 console.log("hello world!")
