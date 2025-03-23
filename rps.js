72be3790cc0ac43532d6d9b085954f1ba7a9a5b3
Billy Leader <164017159+bleader14@users.noreply.github.com>
2025-03-23 09:40:39 +0000

    Add functionality to play 5 rounds
    Added functionality to keep track of user score,
    logic to play a round and the parent function
    which plays 5 rounds and keeps a score

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+
+let userScore = 0;
+let compScore = 0;
+
 // This function returns a random integer, either 0, 1 or 2 and will correspond to rock paper or scissors
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -7,11 +11,11 @@ function getComputerChoice() {
     let choice = getRandomInt(3);
     switch (choice) {
         case 0:
-            return "Rock";
+            return "ROCK";
         case 1:
-            return "Paper";
+            return "PAPER";
         case 2:
-            return "Scissors";
+            return "SCISSORS";
         
         default:
             return "error";
@@ -19,34 +23,78 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Make your choice");
-    let compChoice = getComputerChoice();
+    let choice = prompt("Make your choice").toUpperCase();
+    if (choice === "ROCK" || choice === "PAPER" || choice === "SCISSORS") {
+        return choice
+    } else {
+        return "invalid"
+    }
+}
+
+function getScore() {
+    return `Human = ${userScore} Computer = ${compScore}`;
+}
+
+function playRound(humanChoice, compChoice) {
+    let winner = ""
+    if (humanChoice === "invalid") {
+        return "Invalid user choice"
+    }
 
     if (humanChoice === compChoice) {
-        return "draw";
+        return `The result is a draw: ${getScore()}` ;
     }
 
     switch (humanChoice) {
-        case "Rock":
-            if (compChoice === "Paper") {
-                return "Computer wins";
+        case "ROCK":
+            if (compChoice === "PAPER") {
+                ++compScore;
+                winner = "Computer";
             } else {
-                return "User wins";
+                ++userScore;
+                winner = "User";
             }
-        case "Paper":
-            if (compChoice === "Scissors") {
-                return "Computer wins";
+            break
+        case "PAPER":
+            if (compChoice === "SCISSORS") {
+                ++compScore;
+                winner = "Computer";
             } else {
-                return "User wins";
+                ++userScore;
+                winner = "User";
             }
-        case "Scissors":
-            if (compChoice === "Rock") {
-                return "Computer wins";
+            break
+        case "SCISSORS":
+            if (compChoice === "ROCK") {
+                ++compScore;
+                winner = "Computer";
             } else {
-                return "User wins";
-            } 
-        default:
-            return "No valid option entered";
+                ++userScore;
+                winner = "User";
+            }
+            break
+    }
+    return `${winner} wins this round: ${getScore()}`;
+}
+
+function playGame() {
+    //initialise scores
+    userScore = 0;
+    compScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let compChoice = getComputerChoice();
+        playRound(humanChoice, compChoice);
+    }
+    
+    console.log(getScore());
+
+    if (userScore === compScore) {
+        return "The match is a draw";
+    } else if (userScore > compScore) {
+        return "You win this match";
+    } else {
+        return "The computer wins this match";
     }
 }
-console.log("hello world!")
